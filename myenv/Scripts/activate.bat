8fce88e4d433e7257631a90e36e06265cdfdee2e
JUAN GARZON <jmgarzonv@eafit.edu.co>
2024-08-01 12:18:34 -0500

    Se modifico el index por historia de usuario modificada por juan garzon

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\JUAN\Desktop\Extracurrifit\P1extracurrifit\myenv
+set VIRTUAL_ENV=C:\Users\JUAN GARZON\Desktop\Proyectodj\P1extracurrifit\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
