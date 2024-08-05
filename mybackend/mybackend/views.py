e9b433cbd5d5c63292073aefd0871c3f88d9878e
lemorenog <lemorenog@eafit.edu.co>
2024-08-04 23:28:41 -0500

    Funcion historia_usuario creada

--- mybackend/mybackend/views.py
+++ mybackend/mybackend/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
-def hello_world(request):
-    data = {'message': 'Hello, world!'}
+def historia_usuario(request):
+    data = {'Historia de usuario seleccionada': 'Como estudiante, quiero hacer clic en una actividad para ver más detalles, para obtener información completa sobre eventos y oportunidades.'}
     return JsonResponse(data)
 
