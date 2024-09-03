d7fb7c48c6fa9525902cb00c40ea64970ace85fb
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2024-09-03 18:34:12 -0500

    Feature: Add semilleros.json
    - Add semilleros.json
    - Add semilleros.css
    - Change semilleros.jsx

--- myfrontend/src/screens/semilleros.jsx
+++ myfrontend/src/screens/semilleros.jsx
@@ -1,16 +1,30 @@
 import Header from "../includes/header";
 import Footer from "../includes/footer";
+import semillerosData from "../util/semilleros.json"
+import '../styles/semilleros.css';
 
-function Semill (){
+function Semi (){
 
     return(
         <>
         <Header/>
-            <div>
-                <h1>Seccion semilleros</h1>
+            <div className="semilleros-container">
+                <div className="text-container">
+                <h1>Estos son los semilleros</h1>
+                </div>
+                <div className="semilleros-list">
+                    {semillerosData.map((semillero, index) => (
+                    <div key={index} className="semillero-card">
+                        <h5>{semillero.nombre}</h5>
+                        <h6>{semillero.escuela}</h6>
+                        <p>{semillero.descripcion}</p>
+                        <button className="ver-mas">Ver más</button>
+                    </div>
+                    ))}
+                </div>
             </div>
         <Footer/>
         </>
     )
 }
-export default Semill;
+export default Semi;
