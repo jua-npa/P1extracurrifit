5c99857d80879acb379eb4046d794253b7475885
juanrobles05 <116527097+juanrobles05@users.noreply.github.com>
2024-09-30 11:31:09 -0500

    Fix: the call of ia function

--- mybackend/semilleros/urls.py
+++ mybackend/semilleros/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SemilleroListView, semillero_detail
+from .views import SemilleroListView, semillero_detail, resumen_view
 
 urlpatterns = [
     path('semilleros/', SemilleroListView.as_view(), name='semilleros-list'),
     path('semilleros/<int:id>/', semillero_detail, name='semillero_detail'),
+    path('resumen/', resumen_view, name='resumen_view'),
 ]
